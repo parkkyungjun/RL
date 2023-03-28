4eb53fc41d07eeb595dc05cf3abe56822dabe8ef
pkj <rudwns708.14564@gmail.com>
2023-03-28 18:11:28 +0900

    fix error

--- agent.py
+++ agent.py
@@ -4,6 +4,7 @@ import numpy as np
 from collections import deque
 from game import SnakeGameAI, Direction, Point
 from model import Linear_QNet, QTrainer
+from helper import plot
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
@@ -13,11 +14,12 @@ class Agent:
 
     def __init__(self):
         self.n_games = 0
-        self.epsilon = 0
-        self.gamma = 0.9
-        self.memory = deque(maxlen=MAX_MEMORY)
+        self.epsilon = 0 # randomness
+        self.gamma = 0.9 # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY) # popleft()
         self.model = Linear_QNet(11, 256, 3)
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+
 
     def get_state(self, game):
         head = game.snake[0]
@@ -61,29 +63,33 @@ class Agent:
             game.food.x > game.head.x,  # food right
             game.food.y < game.head.y,  # food up
             game.food.y > game.head.y  # food down
-        ]
+            ]
 
         return np.array(state, dtype=int)
+
     def remember(self, state, action, reward, next_state, done):
-        self.memory.append(state, action, reward, next_state, done) #popleft if max reached
+        self.memory.append((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is reached
 
-    def train_short_memory(self, state, action, reward, next_state, done):
-        self.trainer.train_step(state, action, reward, next_state, done)
-
-    def train_long_memory(self, states, actions, rewards, next_states, dones):
+    def train_long_memory(self):
         if len(self.memory) > BATCH_SIZE:
-            mini_sample = random.sample(self.memory, BATCH_SIZE)
+            mini_sample = random.sample(self.memory, BATCH_SIZE) # list of tuples
         else:
             mini_sample = self.memory
 
         states, actions, rewards, next_states, dones = zip(*mini_sample)
         self.trainer.train_step(states, actions, rewards, next_states, dones)
+        #for state, action, reward, nexrt_state, done in mini_sample:
+        #    self.trainer.train_step(state, action, reward, next_state, done)
+
+    def train_short_memory(self, state, action, reward, next_state, done):
+        self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self, state):
+        # random moves: tradeoff exploration / exploitation
         self.epsilon = 80 - self.n_games
         final_move = [0,0,0]
-        if self.epsilon > random.randint(0, 200):
-            move = random.radint(0,2)
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 2)
             final_move[move] = 1
         else:
             state0 = torch.tensor(state, dtype=torch.float)
@@ -92,6 +98,7 @@ class Agent:
             final_move[move] = 1
 
         return final_move
+
 
 def train():
     plot_scores = []
@@ -125,11 +132,16 @@ def train():
 
             if score > record:
                 record = score
-                # save model 
+                agent.model.save()
 
             print('Game', agent.n_games, 'Score', score, 'Record:', record)
 
-            # plot
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
+
 
 if __name__ == '__main__':
     train()
--- model.py
+++ model.py
@@ -9,12 +9,11 @@ class Linear_QNet(nn.Module):
         super().__init__()
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, output_size)
-        self.relu = F.ReLU()
 
     def forward(self, x):
-        x = self.relu(self.linear1(x))
+        x = F.relu(self.linear1(x))
         x = self.linear2(x)
-        return x 
+        return x
 
     def save(self, file_name='model.pth', n_games=0, epoch=0, optimizer=None):
         model_folder_path = './model'
@@ -28,7 +27,7 @@ class Linear_QNet(nn.Module):
                     'optimizer_state_dict': optimizer.state_dict(),
                     'n_games': n_games,
                     }, file_name)
-        #torch.save(self.state_dict(), file_name)
+        # torch.save(self.state_dict(), file_name)
 
 class QTrainer:
     def __init__(self, model, lr, gamma):
@@ -44,12 +43,13 @@ class QTrainer:
         reward = torch.tensor(reward, dtype=torch.float)
         next_state = torch.tensor(next_state, dtype=torch.float)
 
-        if len(state) == 1:
+        if len(state.shape) == 1:
             state = torch.unsqueeze(state, 0)
             action = torch.unsqueeze(action, 0)
             reward = torch.unsqueeze(reward, 0)
             next_state = torch.unsqueeze(next_state, 0)
             done = (done, )
+            
 
         # 1. predicted Q values with current state
         pred = self.model(state)
